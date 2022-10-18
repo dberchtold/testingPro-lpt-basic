825b108170995c159c8d44e560c051cde520f620
Dietmar <dietmar.berchtold@nagarro.com>
2022-10-18 10:27:10 +0200

    add bat file

---
+++ setup/pull_images.bat
@@ -0,0 +1,6 @@
+docker pull dberchtold/petclinic-rest-apm:latest
+docker pull dberchtold/petclinic-gui-apm:latest
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.15.2
+docker pull docker.elastic.co/kibana/kibana:7.15.2
+docker pull docker.elastic.co/apm/apm-server:7.15.2
+docker pull grafana/grafana:9.1.5
