89749a1b7dfd91a3c6e0a2fe14a43780953e438b
Dietmar <dietmar.berchtold@nagarro.com>
2022-10-19 16:44:11 +0200

    updates for day 2

--- runners/run-petclinic-rest-apm.cmd
+++ runners/run-petclinic-rest-apm.cmd
@@ -2,6 +2,6 @@ docker run --rm --detach ^
     --name petclinc-rest-apm ^
     -p 9966:9966 ^
     --cpus 1 ^
-    --memory "1024m" ^
+    --memory "400m" ^
     --env ELASTIC_APM_SERVICE_NAME=petclinic-rest ^
     dberchtold/petclinic-rest-apm:latest
--- runners/run-petclinic-rest.cmd
+++ runners/run-petclinic-rest.cmd
@@ -1,7 +1,7 @@
 docker run --rm --detach ^
     --name petclinc-rest ^
     -p 9966:9966 ^
-    --cpus 1 ^
-    --memory "1024m" ^
+    --cpus 0.5 ^
+    --memory "300m" ^
     dberchtold/petclinic-rest-apm:latest ^
     java -jar spring-petclinic.jar
